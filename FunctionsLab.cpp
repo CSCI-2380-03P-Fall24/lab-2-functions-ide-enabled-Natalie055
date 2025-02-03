cbc9e75284a042b1fd858b40629cf517bfdb1bc4
Natalie <147546336+Natalie055@users.noreply.github.com>
2025-02-03 02:16:41 +0000

    lab 2

--- FunctionsLab.cpp
+++ FunctionsLab.cpp
@@ -2,16 +2,20 @@
 #include <string>
 using namespace std; // making life easier so that we do not need to use std::cin , std::cout, etc.
 
-
 //Write the function called locateSmallest in the given interval such that 
 // output : int: index of the smallest value in the array
 // input : an array of int, the starting index of search, the ending point of searchspace
 // function: find the location (i.e index) of the smallest value of array in the given interval
 // Note that I don't want the smallest value
 
-
 int locateSmallest(int array[], int start, int end){
-	return -1; // fix this!
+	int smallestValue = start;
+	for(int i = start + 1; i <= end; i++) {
+if(array[i] < array[smallestValue]) {
+	smallestValue = i;
+}
+}
+return smallestValue;
 }
 
 //Write the function called replaceVariable in the given interval such that 
@@ -21,7 +25,11 @@ int locateSmallest(int array[], int start, int end){
 //     an int which is the ending point of searchspace
 // function: Search in the array from given start to end index and replace the given target value with 1000
 void replaceVariable() {
-	return;
+	for(int i = start; i <= end; i++) {
+		if(array[i] == target) {
+			array[i] == 1000;
+		}
+	}
 }
 
 
@@ -30,15 +38,11 @@ void replaceVariable() {
 //output: nothing
 //input: the array of integers and its size
 void printArray() {
-	return;
+	for(int i = 0; i < size; i++) {
+		cout << array[i] << " ";
+	}
+	cout << endl;
 }
-
-
-
-
-
-
-
 
 //Write a function called sumOdds in the given interval such that 
 // output : integer
@@ -47,7 +51,12 @@ void printArray() {
 //Hint: % is used for modulo operation. 
 //     n%m gives you the remainder after dividing n to m. for example 7%3 = 1 because 7 = 3*2 + 1, then 1 is the remainder.
 
-
 int sumOdds(int array[], int start, int end){
-	return -1;
+	int sum = 0;
+	for(int i = start; i <= end; i++) {
+		if(array[i] % 2 != 0) {
+			sum += array[i];
+		}
+	}
+	return sum;
 }
--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,5 @@
 // write any code to test out your functions here
-// run comman "make demo" to run this code
+// run command "make demo" to run this code
 
 int main() {
     return 0;
